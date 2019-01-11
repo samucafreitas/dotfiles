590c14a7258b91d0d54b79b60a1dfb6b4e732136
Samuel Freitas <samucaof@hotmail.com>
2019-01-11 10:46:22 -0200

    Fixed font config

--- .i3/lbar/loadbars.sh
+++ .i3/lbar/loadbars.sh
@@ -8,7 +8,7 @@ fi
 # kill 0: kill all processes in the current process
 trap 'trap - TERM; kill 0' INT TERM QUIT EXIT
 
-font="FantasqueSansMono-10"
+font="FantasqueSansMono Nerd Font-10"
 icon_font="FontAwesome-10"
 fg_color="#ffffff"
 bg_color="#20202050" #BGA
